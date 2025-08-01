a7224ae9f406081f5250317b7f0d71b66b3d9fe2
nyyakko <nyakonyns@gmail.com>
2025-08-01 19:05:29 -0300

    fix implementation

--- V12/source/Main.cpp
+++ V12/source/Main.cpp
@@ -8,149 +8,329 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <ranges>
 #include <span>
 #include <stack>
+#include <variant>
 
 using namespace liberror;
 using namespace libcoro;
 
-ENUM_CLASS(Instruction,
-    PUSH,
-    PUSHI,
+template <class ... T>
+struct Visitor : T ... { using T::operator()...; };
+
+enum class Instruction
+{
+    PUSHA,
+    PUSHB,
     POP,
-    CALL,
-    CALLI,
+    CALLA,
+    CALLB,
     RET
-);
+};
 
-ENUM_CLASS(Intrinsic, PRINTLN);
+enum class Intrinsic { PRINT, PRINTLN };
 
-ENUM_CLASS(Stack, FRAME, ARGUMENT, SCOPE)
+class Memory
+{
+public:
+    explicit Memory(size_t sizeInBytes)
+    {
+        memory_.reserve(sizeInBytes);
+    }
+
+public:
+    // cppcheck-suppress [functionStatic, constParameterReference]
+    auto& get(this auto& self) { return self.memory_; }
+
+    void push(uint8_t data)
+    {
+        memory_.push_back(data);
+    }
+
+    std::span<uint8_t> fetch(int32_t offset, int32_t index, int32_t sizeInBytes)
+    {
+        return { memory_.data() + offset + index, size_t(sizeInBytes) };
+    }
+
+    uint8_t fetch(int32_t offset, int32_t index)
+    {
+        return fetch(offset, index, 1)[0];
+    }
+
+private:
+    std::vector<uint8_t> memory_;
+};
+
+struct Program
+{
+    int32_t address;
+    int32_t dataSegmentStart;
+    int32_t codeSegmentStart;
+    int32_t entryPoint;
+    int32_t size;
+};
+
+enum class Section { ARGUMENT, SCOPE, DATA };
+
+struct Constant
+{
+    int data;
+};
+
+struct Relative
+{
+    Section source;
+    int data;
+};
+
+using Value = std::variant<Constant, Relative>;
+
+struct Frame
+{
+    int32_t returnAddress;
+
+    std::vector<Value> argument;
+    std::vector<Value> scope;
+};
 
 class Machine
 {
 public:
-    explicit Machine(std::vector<uint8_t> const& program)
-        : program_(program)
-        , programCounter_()
-        , frame_()
-        , argument_()
-        , scope_()
-    {}
+    explicit Machine()
+        : programCounter_()
+        , stack_()
+        , memory_(8 * 1024 * 1024)
+        , program_()
+    {
+    }
 
 public:
+    Result<void> load(std::vector<uint8_t> const& program);
     Result<void> execute();
+
+    Result<uint8_t> tick()
+    {
+        if (program_.address + programCounter_ > program_.address + program_.size)
+        {
+            return make_error("Tried to advance past program size");
+        }
+
+        return memory_.fetch(program_.address, programCounter_++);
+    }
 
 private:
     Result<void> ret();
     Result<void> call();
-    Result<void> calli(Intrinsic intrinsic);
-    Result<void> pop();
-    Result<void> push(int value, Stack destination);
-    Result<void> pushi(Stack source, int index, Stack destination);
-
-    Result<int> fetch(Stack source, int index = -1)
-    {
-        switch (source)
-        {
-        case Stack::FRAME: assert("UNIMPLEMENTED" && false);
-        case Stack::ARGUMENT: {
-            auto value = argument_.top();
-            argument_.pop();
-            return value;
-        }
-        case Stack::SCOPE: return scope_.at(static_cast<size_t>(index));
-        }
-
-        return make_error("Tried to fetch from unknown location {}", source.to_int());
-    }
-
-    Result<uint8_t> advance()
-    {
-        if (programCounter_ >= program_.size())
-        {
-            return make_error("Tried to advance past the program size");
-        }
-
-        return program_.at(programCounter_++);
-    }
+    Result<void> call(Intrinsic intrinsic);
+    Result<void> pop(Section source);
+    Result<void> push(Value value, Section destination);
 
 private:
-    std::vector<uint8_t> program_;
-    size_t programCounter_;
+    int32_t programCounter_;
+    std::stack<Frame> stack_;
+    Memory memory_;
 
-    std::vector<int> frame_;
-    std::stack<int> argument_;
-    std::vector<int> scope_;
+    Program program_;
 };
 
-Result<void> Machine::calli(Intrinsic intrinsic)
-{
-    switch (intrinsic)
-    {
-    case Intrinsic::PRINTLN: {
-        fmt::println("{}", TRY(fetch(Stack::ARGUMENT)));
-        return {};
-    }
-    }
 
-    return make_error("Tried to call unknown intrinsic {}", intrinsic.to_int());
+int32_t bytes_2_int(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+{
+    return static_cast<int>(a << 24 | b << 16 | c << 8  | d << 0);
+};
+
+int32_t bytes_2_int(std::span<uint8_t> bytes)
+{
+    return bytes_2_int(bytes[0], bytes[1], bytes[2], bytes[3]);
 }
 
-Result<void> Machine::push(int value, Stack destination)
+Result<void> Machine::load(std::vector<uint8_t> const& program)
+{
+    program_ = {
+        .address = static_cast<int32_t>(memory_.get().size()),
+        .dataSegmentStart = bytes_2_int(program.at(0), program.at(1), program.at(2), program.at(3)),
+        .codeSegmentStart = bytes_2_int(program.at(4), program.at(5), program.at(6), program.at(7)),
+        .entryPoint       = bytes_2_int(program.at(8), program.at(9), program.at(10), program.at(11)),
+        .size = static_cast<int32_t>(program.size() - (sizeof(Program) - 8))
+    };
+
+    for (auto const& byte : program | std::views::drop(sizeof(Program) - 8))
+    {
+        memory_.push(byte);
+    }
+
+    programCounter_ = program_.entryPoint;
+
+    return {};
+}
+
+Result<void> Machine::ret()
+{
+    programCounter_ = stack_.top().returnAddress;
+    stack_.pop();
+    return {};
+}
+
+Result<void> Machine::call()
+{
+    Frame frame {};
+    frame.returnAddress = programCounter_;
+
+    while (!stack_.top().argument.empty())
+    {
+        auto argument = stack_.top().argument.back();
+        frame.scope.push_back(argument);
+        stack_.top().argument.pop_back();
+    }
+
+    stack_.push(frame);
+
+    programCounter_ = TRY(tick());
+
+    return {};
+
+}
+
+Result<void> Machine::call(Intrinsic intrinsic)
+{
+    std::vector<Value> parameters {};
+
+    while (!stack_.top().argument.empty())
+    {
+        auto argument = stack_.top().argument.back();
+        parameters.push_back(argument);
+        stack_.top().argument.pop_back();
+    }
+
+    switch (intrinsic)
+    {
+    case Intrinsic::PRINT: {
+        [&] (this auto&& self, Value value) -> void {
+            std::visit(Visitor {
+                [&] (Constant value) { fmt::print("{}", value.data); },
+                [&] (Relative value) {
+                    switch (value.source)
+                    {
+                    case Section::SCOPE: {
+                        self(stack_.top().scope.at(size_t(value.data)));
+                        break;
+                    }
+                    case Section::DATA: {
+                        auto offset = program_.address + program_.dataSegmentStart;
+                        auto bytes = memory_.fetch(offset + 4, value.data, offset);
+                        std::string_view data(reinterpret_cast<char*>(bytes.data()), size_t(offset));
+                        fmt::print("{}", data);
+                        break;
+                    }
+                    case Section::ARGUMENT: assert("UNREACHABLE" && false);
+                    }
+                }
+            }, value);
+        }(parameters.at(0));
+        break;
+    }
+    case Intrinsic::PRINTLN: {
+        [&] (this auto&& self, Value value) -> void {
+            std::visit(Visitor {
+                [&] (Constant value) { fmt::println("{}", value.data); },
+                [&] (Relative value) {
+                    switch (value.source)
+                    {
+                    case Section::SCOPE: {
+                        self(stack_.top().scope.at(size_t(value.data)));
+                        break;
+                    }
+                    case Section::DATA: {
+                        auto offset = program_.address + program_.dataSegmentStart;
+                        auto size = bytes_2_int(memory_.fetch(offset, value.data, 4));
+                        auto bytes = memory_.fetch(offset + 4, value.data, size);
+                        std::string_view data(reinterpret_cast<char*>(bytes.data()), size_t(size));
+                        fmt::println("{}", data);
+                        break;
+                    }
+                    case Section::ARGUMENT: assert("UNREACHABLE" && false);
+                    }
+                }
+            }, value);
+        }(parameters.at(0));
+        break;
+    }
+    }
+
+    return {};
+}
+
+Result<void> Machine::push(Value value, Section destination)
 {
     switch (destination)
     {
-    case Stack::FRAME: {
-        assert("UNIMPLEMENTED" && false);
-        return {};
+    case Section::ARGUMENT: {
+        stack_.top().argument.push_back(value);
+        break;
     }
-    case Stack::ARGUMENT: {
-        argument_.push(value);
-        return {};
+    case Section::SCOPE: {
+        stack_.top().scope.push_back(value);
+        break;
     }
-    case Stack::SCOPE: {
-        scope_.push_back(value);
-        return {};
+    case Section::DATA: {
+        return make_error("ACCESS VIOLATION: Tried to push to .DATA section");
     }
     }
 
-    return make_error("Tried to push to unknown location {}", destination.to_int());
+    return {};
 }
 
-Result<void> Machine::pushi(Stack source, int index, Stack destination)
+Result<void> Machine::pop(Section source)
 {
-    return push(TRY(fetch(source, index)), destination);
+    switch (source)
+    {
+    case Section::ARGUMENT: {
+        stack_.top().argument.pop_back();
+        break;
+    }
+    case Section::SCOPE: {
+        stack_.top().scope.pop_back();
+        break;
+    }
+    case Section::DATA: {
+        return make_error("ACCESS VIOLATION: Tried to pop from .DATA section");
+    }
+    }
+
+    return {};
 }
 
 Result<void> Machine::execute()
 {
-    while (programCounter_ < program_.size())
+    stack_.push({});
+
+    while (!stack_.empty())
     {
-        switch (TRY(advance()))
+        switch (Instruction(TRY(tick())))
         {
-        case Instruction::PUSH: {
-            TRY(push(TRY(advance()), Stack::from_int(TRY(advance()))));
+        case Instruction::PUSHA: {
+            TRY(push(Constant { TRY(tick()) }, Section(TRY(tick()))));
             break;
         }
-        case Instruction::PUSHI: {
-            TRY(pushi(Stack::from_int(TRY(advance())), TRY(advance()), Stack::from_int(TRY(advance()))));
+        case Instruction::PUSHB: {
+            TRY(push(Relative { Section(TRY(tick())), TRY(tick()) }, Section(TRY(tick()))));
             break;
         }
         case Instruction::POP: {
-            assert("UNIMPLEMENTED" && false);
+            TRY(pop(Section(TRY(tick()))));
             break;
         }
-        case Instruction::CALL: {
-            assert("UNIMPLEMENTED" && false);
+        case Instruction::CALLA: {
+            TRY(call());
             break;
         }
-        case Instruction::CALLI: {
-            TRY(calli(Intrinsic::from_int(TRY(advance()))));
+        case Instruction::CALLB: {
+            TRY(call(Intrinsic(TRY(tick()))));
             break;
         }
         case Instruction::RET: {
-            assert("UNIMPLEMENTED" && false);
+            TRY(ret());
             break;
         }
         }
@@ -191,7 +371,9 @@ Result<void> safe_main(std::span<char const*> arguments)
         program.push_back(instruction);
     }
 
-    Machine machine(program);
+    Machine machine;
+
+    TRY(machine.load(program));
     TRY(machine.execute());
 
     return {};
